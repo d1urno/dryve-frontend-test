d20a3aae34a10f6634dcc39766913b3f4500f0bf
Pablo Miceli <d1urno@gmx.com>
2020-11-26 18:34:51 -0300

    types(models): create car and client types

---
+++ src/store/store-constants.ts
@@ -0,0 +1,22 @@
+// API status
+export const API_REQUEST = 'API_REQUEST'
+export const API_SUCCESS = 'API_SUCCESS'
+export const API_ERROR = 'API_ERROR'
+export enum RequestStatus {
+  SUCCESS,
+  LOADING,
+  ERROR
+}
+
+// Cars
+export const FETCH_CARS = 'FETCH_CARS'
+
+// Clients
+export const ADD_CLIENT = 'ADD_CLIENT'
+export const EDIT_CLIENT = 'EDIT_CLIENT'
+export const DELETE_CLIENT = 'REMOVE_CLIENT'
+
+// Getters
+export const STATUS = 'STATUS'
+export const CARS = 'CARS'
+export const CLIENTS = 'CLIENTS'
