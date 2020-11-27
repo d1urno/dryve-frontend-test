fde435a46b15b74af9fc24f72f8a9d5f067ffe9c
Pablo Miceli <d1urno@gmx.com>
2020-11-27 14:11:05 -0300

    style(fonts): install fonts and text styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -171,11 +171,17 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
+        'Inter',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"'
+      ],
+      body: [
         'Roboto',
         '"Helvetica Neue"',
         'Arial',
