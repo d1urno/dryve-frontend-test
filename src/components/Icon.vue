b0073184921fe8170d9bb05c53893ae878b4d086
Pablo Miceli <d1urno@gmx.com>
2020-11-28 22:48:37 -0300

    workflow(icons): set icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <i
+    class="inline-flex items-center justify-center"
+    role="presentation"
+    :aria-labelledby="type"
+  >
+    <component :is="type" v-bind="$attrs" />
+  </i>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineAsyncComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Icon',
+  components: {},
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
