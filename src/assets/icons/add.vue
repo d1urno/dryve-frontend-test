73dc1ba625441383b198b87f6d0b1528ba09578f
Pablo Miceli <d1urno@gmx.com>
2020-11-28 21:56:19 -0300

    chore(icons): add page icons

---
+++ src/assets/icons/add.vue
@@ -0,0 +1,4 @@
+<template>
+  <!--prettier-ignore -->
+  <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="currentColor"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+</template>
---
+++ src/assets/icons/chevron-left.vue
@@ -0,0 +1,4 @@
+<template>
+  <!--prettier-ignore -->
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 24 24"><defs><path id="chevron-left-a" d="M7.41 1.41L6 0 0 6l6 6 1.41-1.41L2.83 6z"/></defs><g fill="none" fill-rule="evenodd" transform="translate(8 6)"><mask id="chevron-left-b" fill="#fff"><use xlink:href="#chevron-left-a"/></mask><g fill="currentColor" mask="url(#chevron-left-b)"><path d="M-21-19h50v50h-50z"/></g></g></svg>
+</template>
