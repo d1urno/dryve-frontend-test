6720ae79b1f64ee570034dc88299c42a33629737
Pablo Miceli <d1urno@gmx.com>
2020-11-30 19:09:58 -0300

    feat(navbar): set logo as link to home

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -2,12 +2,12 @@
   <div
     class="flex items-center justify-between px-6 bg-white border-b-2 border-r-2"
   >
-    <div class="flex items-center space-x-2">
+    <router-link to="/" class="flex items-center space-x-2">
       <img src="/src/assets/logo.svg" alt="" />
       <span class="mt-2 -mb-1 text-xs text-gray-400 uppercase font-body">
         Backoffice
       </span>
-    </div>
+    </router-link>
     <div class="flex space-x-3">
       <router-link to="/" class="pt-1 pl-1 pr-2 -ml-1 -mr-2">
         <icon type="search" class="w-6 h-6 text-black text-opacity-30" />
