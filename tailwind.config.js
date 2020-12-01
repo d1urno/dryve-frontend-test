91b9d25a225b43dd64958a24874359712f05a4a1
Pablo Miceli <d1urno@gmx.com>
2020-11-30 23:23:36 -0300

    build(tailwind): enable tailwindCSS purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.vue'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
